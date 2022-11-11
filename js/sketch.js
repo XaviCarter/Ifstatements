2a003c01b0d94604a6f68145fa6ccc0e92868dca
Xavi Carter <116882813+XaviCarter@users.noreply.github.com>
2022-11-11 13:01:35 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -6,6 +6,37 @@ function setup() {
 function draw() {
     background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    if (mouseX < width/2) {
+        //console.log('LEFT');
+        fill('#24FF00');
+        ellipse(400,400, 200,200)
+        
+       
+        fill('#000000')
+        ellipse(360, 350, 25,25)
+        
+        fill('#000000')
+        ellipse(440, 350, 25,25)
+
+        noFill();
+        arc(400, 425, 100, 100, 0, 650)
+        
+    }
+    
+    if (mouseX > width/2) {
+        //console.log('RIGHT');
+        fill('#FF0000');
+        ellipse(400,400, 200,200)
+        
+       
+        fill('#000000')
+        ellipse(360, 350, 25,25)
+        
+        fill('#000000')
+        ellipse(440, 350, 25,25)
+
+        noFill();
+        arc(400,450, 90, 90, 650, 0)
+    }
+    
 }
